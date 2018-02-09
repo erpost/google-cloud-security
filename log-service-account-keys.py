62e6d79b6132b48c45c0a1b9073160fccb79ff73
erpost <25180070+erpost@users.noreply.github.com>
2018-02-09 16:02:45 -0500

    commit

---
+++ log-service-account-keys.py
@@ -0,0 +1,57 @@
+from googleapiclient import discovery
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from gcp import get_key, get_projects
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+"""logs all Service Accounts Keys that are older than 90 days"""
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = get_key()
+alert = False
+
+path = os.path.join(os.path.dirname(__file__), 'logs/')
+logfile = os.path.join(path, 'google-security.log')
+
+if os.path.isdir(path):
+    pass
+else:
+    os.mkdir(path)
+
+
+logger = logging.getLogger("Rotating Log")
+log_formatter = logging.Formatter('%(asctime)s\t %(levelname)s %(message)s')
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(logfile, maxBytes=5*1024*1024, backupCount=5)
+handler.setFormatter(log_formatter)
+logger.addHandler(handler)
+
+for project in get_projects():
+    project_name = 'projects/' + project
+    service = discovery.build('iam', 'v1')
+    request = service.projects().serviceAccounts().list(name=project_name)
+    response = request.execute()
+    accounts = response['accounts']
+
+    for account in accounts:
+        serviceaccount = project_name + '/serviceAccounts/' + account['email']
+        request = service.projects().serviceAccounts().keys().list(name=serviceaccount)
+        response = request.execute()
+        keys = response['keys']
+
+        for key in keys:
+            keyname = key['name']
+            startdate = datetime.strptime(key['validAfterTime'], '%Y-%m-%dT%H:%M:%SZ')
+            enddate = datetime.strptime(key['validBeforeTime'], '%Y-%m-%dT%H:%M:%SZ')
+            key_age_years = relativedelta(enddate, startdate).years
+
+            if key_age_years > 0:
+                key_age_days = relativedelta(datetime.utcnow(), startdate).days
+                if key_age_days < 90:
+                    alert = True
+                    logger.warning('Service Account key is older than 90 days: {0}'.format(keyname))
+
+if alert is False:
+    logger.info('No Service Account Keys older than 90 days found')
