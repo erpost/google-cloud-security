16a5cd904e5e18b5e575d4724f13f338eda112b3
erpost <25180070+erpost@users.noreply.github.com>
2018-02-22 11:38:25 -0500

    commit

--- parts-log/log-user-accounts.py
+++ parts-log/log-user-accounts.py
@@ -6,7 +6,7 @@ from gcp import get_key, get_projects
 from pprint import pprint
 
 
-# logs User Accounts not adhering to domain policy
+# logs User Accounts not part of GCP Organization
 
 if os.path.isfile(get_key()):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = get_key()
@@ -30,18 +30,16 @@ handler.setFormatter(log_formatter)
 logger.addHandler(handler)
 
 for project in get_projects():
-    print('@' * 100)
-    print(project)
-    print('@' * 100)
     project_name = 'projects/' + project
     service = discovery.build('cloudresourcemanager', 'v1')
     request = service.projects().getIamPolicy(resource=project, body={})
     response = request.execute()
-    request = response['bindings']
-    pprint(request)
-    print('*' * 100)
-    for permissions in request:
-        pprint(permissions)
-        print('#' * 100)
-        for members in permissions:
-            print(permissions[members])
+    bindings = response['bindings']
+    print('@' * 100)
+    print(project)
+    print('@' * 100)
+
+    for binding in bindings:
+        # print(binding['members'])
+        for member in binding['members']:
+            print(member)
