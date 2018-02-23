b1a6e67394bddccbc640f5e3e05e288baf0befb9
erpost <25180070+erpost@users.noreply.github.com>
2018-02-23 16:06:14 -0500

    commit

--- parts-remove/remove-default-service-accounts.py
+++ parts-remove/remove-default-service-accounts.py
@@ -4,7 +4,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 from gcp import get_key, get_projects
 
-"""removes all Service Accounts from the Service Account page and logs"""
+# removes all Service Accounts from the Service Account page and logs
 
 if os.path.isfile(get_key()):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = get_key()
