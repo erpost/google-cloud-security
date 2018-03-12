9c765678d13ce3a7002074efcb1645c1cb32a970
erpost <25180070+erpost@users.noreply.github.com>
2018-03-12 15:59:27 -0400

    commit

--- parts-log/log-sql-unsecure-connections.py
+++ parts-log/log-sql-unsecure-connections.py
@@ -5,7 +5,6 @@ from logging.handlers import RotatingFileHandler
 from googleapiclient.errors import HttpError
 from gcp import get_key, get_projects
 
-from pprint import pprint
 
 # Logs all Cloud SQL Databases without enforced SSL Connections
 
@@ -30,37 +29,30 @@ handler = RotatingFileHandler(logfile, maxBytes=5*1024*1024, backupCount=5)
 handler.setFormatter(log_formatter)
 logger.addHandler(handler)
 
-projects = ['allofus-bigquery', 'allofus-forseti']
-
-for project in projects:
-# for project in get_projects():
-#     print(project)
+for project in get_projects():
+    print(project)
 
     try:
         service = discovery.build('sqladmin', 'v1beta4')
         request = service.instances().list(project=project)
         response = request.execute()
-        items = response['items']
 
-        for item in items:
-            print(75 * '*')
-            print(project)
-            if item['name']:
-                print(item['name'])
-                if item['settings']['ipConfiguration']['requireSsl']:
-                    print(item['settings']['ipConfiguration']['requireSsl'])
-                else:
-                    print('Missing SSL Key')
-            # ssl_enforced = item['settings']['ipConfiguration']['requireSsl']
-            # db_name = item['name']
-            # logger.warning('Database "{0}" in Project "{1}" has SSL configured to: "{2}"'.format(db_name, project, ssl_enforced))
+        if 'items' in response:
+            items = response['items']
+            for item in items:
 
-    except KeyError:
-        logger.info('0 Databases in Project "{0}"'.format(project))
+                if 'requireSsl' not in item['settings']['ipConfiguration']:
+                    db_name = item['name']
+                    logger.warning('Database "{0}" in Project "{1}" does not have SSL enforced'.
+                                   format(db_name, project))
+
+        else:
+            logger.info('0 Databases in Project "{0}"'.format(project))
 
     except HttpError as he:
         if he.resp.status == 403:
-            logger.error('Access Denied error.  Check Cloud SQL Permissions on Project "{0}"'.format(project))
+            logger.error('Access Denied.  Check Cloud SQL Permissions on Project "{0}"'.format(project))
 
     except Exception:
-        logger.error('Cloud SQL Unsecure Connections - Unknown error in project "{0}".  Please run manually'.format(project))
+        logger.error('Cloud SQL Unsecure Connections - Unknown error in project "{0}".  '
+                     'Please run manually'.format(project))
