862cc148f4a09f6f4f9166fc3b4b65c0594ea97b
erpost <25180070+erpost@users.noreply.github.com>
2018-04-07 10:32:43 -0400

    commit

--- parts-remove/remove-world-readable-buckets-permissions.py
+++ parts-remove/remove-world-readable-buckets-permissions.py
@@ -1,9 +1,11 @@
 from google.cloud import storage
 from logging.handlers import RotatingFileHandler
-from gcp import get_key, get_projects, send_gmail
+from gcp import get_key, get_projects
 
 import os
 import logging
+import smtplib
+import credentials
 
 
 """Removes Global Permissions from Google Cloud Platform Buckets and sends Email with Bucket and Project Names"""
@@ -34,6 +36,41 @@ handler.setFormatter(log_formatter)
 logger.addHandler(handler)
 
 
+def send_email(subject, body):
+    """send email alert"""
+    logger.info('Sending email')
+    recipient = credentials.get_recipient_email()
+    subject = subject
+
+    # gmail sign-in
+    gmail_sender = credentials.get_sender_email()
+    gmail_passwd = credentials.get_password()
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_sender, gmail_passwd)
+    except smtplib.SMTPAuthenticationError:
+        logger.error('Bad credentials.  Exiting...')
+        exit(1)
+    except Exception as err:
+        logger.error('Gmail failure: {0}'.format(err))
+        exit(1)
+
+    body = '\r\n'.join(['To: %s' % recipient,
+                        'From: %s' % gmail_sender,
+                        'Subject: %s' % subject,
+                        '', body])
+
+    try:
+        server.sendmail(gmail_sender, [recipient], body)
+        logger.info('Email sent!')
+    except Exception as err:
+        logger.error('Sending mail failure: {0}'.format(err))
+
+    server.quit()
+
+
 for project_name in get_projects():
     storage_client = storage.Client(project=project_name)
     buckets = storage_client.list_buckets()
@@ -56,12 +93,12 @@ for project_name in get_projects():
     except Exception as err:
         logger.error(err)
 
-subject = 'Globally Accessible Buckets Found and Fixed!'
+subject_text = 'Globally Accessible Buckets Found and Fixed!'
 body_text = ('Globally accessible permissions were removed on the following Buckets.  '
              'See logs for additional information.\n\n')
 body_text += '\n'.join({'Bucket:\t{0}\nProject:\t{1}\n'.format(key, value) for (key, value) in bucket_dict.items()})
 
 if alert:
-    send_gmail(subject, body_text)
+    send_email(subject_text, body_text)
 else:
     logger.info('No world readable permissions removed')
